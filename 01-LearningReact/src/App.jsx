4fb05da9050b499a51cc0ece1d160675d3cc0946
Uday Surya P <udaysuryap.workspace@gmail.com>
2026-09-26 06:11:33 +0530

    Day 1 of React

---
+++ 01-LearningReact/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+    <div id="dad">
+      <h1 id="child1">Child 1</h1>
+      <h1 id="child2">Child 2</h1>
+    </div>
+    <div id="chacha">
+      <h1 id="child1">Grandchild 1</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
